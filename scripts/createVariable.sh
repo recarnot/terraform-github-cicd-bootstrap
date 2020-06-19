e4e780996621bc8ab41cca293fe33f6a619e43ec
ROMAIN ECARNOT <romain.ecarnot@open-groupe.com>
2020-06-19 12:22:30 +0200

    global update

--- scripts/createVariable.sh
+++ scripts/createVariable.sh
@@ -2,7 +2,7 @@
 
 ESCAPED_VALUE=$(echo $3 | sed -e 's/[]\/$*.^[]/\\&/g');
 
-sed -e "s/my-key/$2/" -e "s/my-hcl/false/" -e "s/my-value/$ESCAPED_VALUE/" -e "s/my-sensitive/$4/" -e "s/my-workspace-id/$TFE_WORKSPACE_ID/" < ./scripts/variable.payload.template  > variable.json
+sed -e "s/T_KEY/$2/" -e "s/my-hcl/false/" -e "s/T_VALUE/$ESCAPED_VALUE/" -e "s/T_SECURED/$4/" -e "s/T_WSID/$TFE_WORKSPACE_ID/" < ./scripts/variable.payload.template  > variable.json
 curl --header "Authorization: Bearer $1" --header "Content-Type: application/vnd.api+json" --data @variable.json "https://app.terraform.io/api/v2/vars"
 
 rm -f variable.json
--- scripts/setupWorkspace.sh
+++ scripts/setupWorkspace.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sed "s/placeholder/$TFE_WORKSPACE_NAME/" < ./scripts/workspace.payload.template > workspace.json
+sed "s/T_WS/$TFE_WORKSPACE_NAME/" < ./scripts/workspace.payload.template > workspace.json
 curl --header "Authorization: Bearer $1" --header "Content-Type: application/vnd.api+json" --request POST --data @workspace.json "https://app.terraform.io/api/v2/organizations/$TFE_ORGANIZATION/workspaces" > workspace_result
 wid=$(cat workspace_result | jq -r .data.id)
 
@@ -9,7 +9,7 @@ echo "TFE_WORKSPACE_NAME is $TFE_WORKSPACE_NAME"
 echo "TFE_WORKSPACE_ID is $wid"
 echo ::set-env name=TFE_WORKSPACE_ID::$wid
 
-sed -e "s/my-orga/$TFE_ORGANIZATION/" -e "s/my-ws/$TFE_WORKSPACE_NAME/" < ./scripts/backend.template > backend.tf
+sed -e "s/T_ORGA/$TFE_ORGANIZATION/" -e "s/T_WS/$TFE_WORKSPACE_NAME/" < ./scripts/backend.template > backend.tf
 
 curl --header "Authorization: Bearer $1" --header "Content-Type: application/vnd.api+json" "https://app.terraform.io/api/v2/vars?filter%5Borganization%5D%5Bname%5D=$TFE_ORGANIZATION&filter%5Bworkspace%5D%5Bname%5D=$TFE_WORKSPACE_NAME" > vars.json
 x=$(cat vars.json | jq -r ".data[].id" | wc -l | awk '{print $1}')
