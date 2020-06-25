eceafc6f70c8b9b0f6b82f6df5f91da51e61d659
ROMAIN ECARNOT <romain.ecarnot@open-groupe.com>
2020-06-25 10:01:01 +0200

    Update outputs

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,29 @@
-output "fullname" {
+output "repository_name" {
+  description = "Name of the repository."
+  value       = github_repository.project.name
+}
+
+output "repository_fullname" {
   description = "A string of the form 'orgname/reponame'."
   value       = github_repository.project.full_name
 }
 
-output "http_clone_url" {
+output "repository_http_clone_url" {
   description = "URL that can be provided to git clone to clone the repository via HTTPS."
   value       = github_repository.project.http_clone_url
 }
 
-output "ssh_clone_url" {
+output "repository_ssh_clone_url" {
   description = "URL that can be provided to git clone to clone the repository via SSH."
   value       = github_repository.project.ssh_clone_url
+}
+
+output "tf_organization" {
+  description = "Terraform organization"
+  value       = var.tf_organization
+}
+
+output "tf_workspace" {
+  description = "Terraform workspace"
+  value       = var.tf_workspace
 }
